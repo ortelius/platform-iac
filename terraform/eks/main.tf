f2339b77b2b1f1f9d29161acfd028be29ffee855
Steve Taylor <steve@deployhub.com>
2026-03-12 15:11:59 -0600

    chore(eks): update pdvd values with infrastructure outputs

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -137,7 +137,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t4g.small"]
+      instance_types = ["t4g.medium"]
       ami_type       = "AL2023_ARM_64_STANDARD"
       capacity_type  = "SPOT"
       min_size       = 2
